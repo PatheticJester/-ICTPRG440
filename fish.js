ccc30cb5975b50e210f023fb692c9d7d34b2141b
PatheticJester <mothandtheflame23567@gmail.com>
2022-08-01 15:59:51 +0930

    first commit - noiframes

--- fish.js
+++ fish.js
@@ -43,26 +43,21 @@ const fish = [
     },
 ];
 
-const searcharry = [1, 2, 3, 4, 5, 6]
+const searcharry = [1, 2, 3, 4, 5, 6];
 
+const keptfish = [];
 
-function FrameWrite(tag, content, mod) {
-    /**
-     * This function serves as a frame handler. Taking three parameters that
-     * allign with the tag, content of the tag and any modification. i.e classes
-     * The handler will do light modification to the tag by adding operators (<> or </>).
-     * The extent of the modifications will depend on whether the user defined parameter mod.
-     * When finished it will write the code to the iFrame.
-     */
-    if (mod == undefined) {
-        endtag = "</" + tag + ">"
-        tag = "<" + tag + ">"
-    } else {
-        endtag = "</" + tag + ">"
-        tag = "<" + tag + " " + mod + ">"
-    }
-    document.getElementById('gameframe')
-        .contentDocument.write(tag, content, endtag)
+points = 0;
+
+div = document.createElement('div');
+div.className = 'playcom';
+div.id = 'playdiv'
+
+function Write(tag, text) {
+    let temp = document.createElement(tag);
+    temp.textContent = text;
+    div.appendChild(temp);
+    document.body.appendChild(div);
 }
 
 function IDman(id, change, target){
@@ -72,61 +67,87 @@ function IDman(id, change, target){
      * as parameters. A small function but I anticpate it will save
      * time.
      */
-        document.getElementById(id)[target] = change
-}
-
-function FishLoop() {
-    points = 0
-    /**
-     * The main loop. Using binary search to compare a random number to
-     * an array and then using that number to find the fish. 
-     * INVESTIGATE ASYNC/Promises FOR DESCIONS
-     */
-        FrameWrite("h1", "Fishing Time!", "class='frametext'")
-        rndnumb = Math.floor(Math.random() * (6 - 1 + 1) + 1)
-        firstIndex  = 0
-        lastIndex   = searcharry.length - 1
-        middleIndex = Math.floor((lastIndex + firstIndex)/2)
-
-        while(searcharry[middleIndex] != rndnumb && firstIndex < lastIndex) {
-            if (rndnumb < searcharry[middleIndex])
-            {
-                lastIndex = middleIndex - 1;
-            } 
-            else if (rndnumb > searcharry[middleIndex])
-            {
-                firstIndex = middleIndex + 1;
-            }
-            middleIndex = Math.floor((lastIndex + firstIndex)/2);
-        }
-
-        result = searcharry[middleIndex = Math.floor((lastIndex + firstIndex)/2)] - 1
-        reply = "You caught a " + fish[(result)].Name
-        FrameWrite("p", reply, "class=playcom")
-        FrameWrite("p", "Would you like to keep it?", "class=playcom")
-
-        document.getElementById("buttonyes").addEventListener("click", () => {
-            points = fish[(result)].PointsKept
-            IDman("outputText", "Points = " + points, "innerHTML")
-            FrameClean()
-            element.removeEventListener("click")
-        })
-    
-        document.getElementById("buttonno").addEventListener("click", () => {
-            points = fish[(result)].PointsFreed
-            IDman("outputText", "Points = " + points, "innerHTML")
-            FrameClean()
-        })
+        document.getElementById(id)[target] = change;
 }
 
 function FishMain() {
-    /** 
-     * Too allow modifcation of content within the iFrame 
-     * and due to how we are handling the iFrame itself 
-     * we need to write the link to it using the frame handler. 
+    /**
+     * The main loop. Using binary search to compare a random number to
+     * an array and then using that number to find the fish. 
      */
-    FrameWrite("link", "", "rel='stylesheet' href='style.css'")
-    IDman("outputText", "Points:", 'innerHTML')
-    IDman("buttonyes", "function() {return(null)};", "onclick" )
-    FishLoop()
+    document.getElementById( "buttonyes" ).setAttribute( "onClick", " " );
+    Write("p", "This text is too allow the node loop to clean the DOM elements");
+    IDman("outputText", "Points: " + points, 'innerHTML');
+    rndnumb = Math.floor(Math.random() * (6 - 1 + 1) + 1);
+    firstIndex  = 0;
+    lastIndex   = searcharry.length - 1;
+    middleIndex = Math.floor((lastIndex + firstIndex)/2);
+    while(searcharry[middleIndex] != rndnumb && firstIndex < lastIndex) {
+        if (rndnumb < searcharry[middleIndex])
+        {
+            lastIndex = middleIndex - 1;
+        } 
+        else if (rndnumb > searcharry[middleIndex])
+        {
+            firstIndex = middleIndex + 1;
+        }
+        middleIndex = Math.floor((lastIndex + firstIndex)/2);
+        }
+        result = searcharry[middleIndex = Math.floor((lastIndex + firstIndex)/2)] - 1;
+    reply = "You caught a " + fish[(result)].Name;
+    
+    node = document.getElementById("playdiv");
+    while (node.lastElementChild) {
+       node.removeChild(node.lastElementChild);
+    }
+    
+    keptfish.push(fish[(result)].Name);
+    Write("h1", "It's Fishing time!");
+    Write("p", reply);
+    Write("p", "Would you like to keep it?");
+    document.getElementById("buttonyes").addEventListener("click", buttonhandler);
+    document.getElementById("buttonno").addEventListener("click", buttonhandler);
+}
+
+function buttonhandler(decision) {
+    if (decision.path[0].className == "button"){
+        points = points + fish[(result)].PointsKept;
+        IDman("outputText", "Points: " + points, "innerHTML");
+    }
+    if (decision.path[0].className == "nobutton"){
+        keptfish.pop();
+        points = points + fish[(result)].PointsFreed;
+        IDman("outputText", "Points = " + points, "innerHTML");
+    }
+    document.getElementById("buttonno").removeEventListener("click", buttonhandler);
+    document.getElementById("buttonyes").removeEventListener("click", buttonhandler);
+    ending();
+}
+
+function ending() {
+    div = document.getElementById("playdiv");
+    while (div.lastElementChild) {
+        div.removeChild(div.lastElementChild);
+    }
+    Write("h1", "Would you like to fish again?");
+    document.getElementById("buttonyes").addEventListener("click", FishMain);
+    document.getElementById("buttonno").addEventListener("click", finish);
+}
+
+function finish() {
+    div = document.getElementById("playdiv");
+    while (div.lastElementChild) {
+        div.removeChild(div.lastElementChild);
+    }
+
+    Write("h1", "You caught:")
+    for (x = 0; x < keptfish.length; x++)
+    {
+        Write("p", keptfish[x]);
+    }
+    if (points <= 99){
+        Write("h2", "Less than 100 points. Too bad! ):")
+    } else if (points >= 100) {
+        Write("h2" , "100+ points! Well done!")
+    }
 }
